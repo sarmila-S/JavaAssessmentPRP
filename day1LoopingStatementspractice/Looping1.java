bf3f45cc3652602b5e55be48fffd6fbf4e6735ee
Sarmila Sadhasivam <55251743+sarmila-S@users.noreply.github.com>
2020-06-17 12:42:21 +0530

    Add files via upload

---
+++ day1LoopingStatementspractice/Looping1.java
@@ -0,0 +1,10 @@
+//print numbers from 1,2,....,99,100
+package day1;
+
+public class Looping1 {
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++)
+			System.out.print(i+" ");
+	}
+
+}
---
+++ day1LoopingStatementspractice/Looping4.java
@@ -0,0 +1,16 @@
+//print baclwards from 0 to 100
+package day1;
+
+public class Looping4 {
+
+	public static void main(String[] args) {
+		int a=100;
+		do
+		{
+			System.out.print(a+" ");
+			a--;
+		}while(a>=0);
+
+	}
+
+}
